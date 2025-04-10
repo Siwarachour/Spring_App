2028421d2dfbeb22160be8cba1e0d9eacb42db2a
Siwarachour <siwar.achour@esprit.tn>
2025-04-10 19:44:34 +0100

    ajout lien front office

--- src/main/java/tn/esprit/back/configurations/CorsConfig.java
+++ src/main/java/tn/esprit/back/configurations/CorsConfig.java
@@ -21,6 +21,7 @@ public class CorsConfig {
             CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of("http://localhost:4200"));
+            config.setAllowedOrigins(List.of("http://localhost:4201"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","OPTIONS"));
             config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
